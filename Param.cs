71ef1846334752f3d9e5dfab81f55ff6208080f5
emi <emi@LAPTOP-TNNL42IO>
2021-10-28 21:19:35 +0100

    Add project files.

---
+++ Param.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Param
+    {
+        public float height { get; set; }
+        public float width { get; set; }
+
+        public override string ToString()
+        {
+            return "heigth=" + this.height + " width=" + this.width;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Util> myList = new List<Util>();
+            bool hasMoreThen3 = false;
+            for (int i = 0; i < 10; i++)
+            {
+                hasMoreThen3 = myList != null && myList.Count > 3;
+                Util u = new Util(new Param() { height = 1.76f, width = 77.3f });
+                myList.Add(u);
+
+                if (hasMoreThen3)
+                    Console.WriteLine(i);
+               
+                
+            }
+            
+        }
+    }
+}
